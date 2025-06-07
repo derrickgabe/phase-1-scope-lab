837f3c50cdee80bd197672f68bc4fb3370227751
Derrick <derrickg844@gmail.com>
2025-06-07 23:03:16 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+ var customerName = "bob";
+ var customerEmail = "bob@example.com";
+ var customerAddress = "123 Main St, Anytown, USA";
+    var bestCustomer = {
+        name: customerName,
+        email: customerEmail,
+        address: customerAddress
+    };
+    console.log(bestCustomer);
+    console.log("Best customer email is: " + bestCustomer.email);
+    console.log("Best customer address is: " + bestCustomer.address);
+    console.log("Best customer is: " + bestCustomer.name);
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer(name) {                      
+    bestCustomer = name;
+}   
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "Alice";
+    leastFavoriteCustomer = "Bob"; // This will throw an error because leastFavoriteCustomer is a constant
+}   
